762adec8be9b2652055cb9907c608d2304468b2e
Lynx <mandala.vijay.surya@gmail.com>
2025-03-10 00:44:50 +0530

    feat: Add api-gateway

---
+++ src/test/java/org/theleakycauldron/nimbusapi/NimbusapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.theleakycauldron.nimbusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimbusapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
